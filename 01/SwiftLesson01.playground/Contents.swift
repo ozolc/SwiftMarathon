b82b34464843ed1debc6436abafbfee2fbb3230d
Maksim Nosov <ozolc@yandex.ru>
2018-05-26 08:12:22 +0300

    SwiftLessonHomeWork01

---
+++ 01/SwiftLesson01.playground/Contents.swift
@@ -0,0 +1,10 @@
+
+let firstName = "Maksim"
+let secondName = "Nosov"
+let age = 34
+let height = "178 cm"
+let weight = "78 kg"
+
+print("Student name is \(firstName).")
+print("Student surname is \(secondName).")
+print("His age is \(age).\nStudent's height is \(height) and weight is \(weight).")
